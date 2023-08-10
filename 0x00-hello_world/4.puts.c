a5ceeb7b6da237a74e96867de2207e164883b2ec
adams miracle <adamsmiracle0@gmail.com>
2023-08-10 22:41:51 +0000

    task 4

---
+++ 0x00-hello_world/4.puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - Entry function to program
+  *
+  * Return: void
+  */
+
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle \n");
+	return (0);
+}
