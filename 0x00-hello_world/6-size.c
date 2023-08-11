913f67817fe9747ec90040de196152ae8302f77b
adams miracle <adamsmiracle0@gmail.com>
2023-08-11 23:50:31 +0000

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,15 +13,15 @@ float c;
 long int d;
 long long int e;
 
-printf("Size of char is: %lu byte(s)",(unsigned long)sizeof(a));
+printf("Size of char is: %lu byte(s)\n",(unsigned long)sizeof(a));
 
-printf("Size of int is: %lu byte(s)", (unsigned long)sizeof(b));
+printf("Size of int is: %lu byte(s)\n", (unsigned long)sizeof(b));
 
-printf("Sizeof long int is : %lu byte(s)", (unsigned long)sizeof(d));
+printf("Sizeof long int is : %lu byte(s)\n", (unsigned long)sizeof(d));
 
-printf("Size of float is : %lu byte(s)" (unsigned long)sizeof(c));
+printf("Size of float is : %lu byte(s)\n" (unsigned long)sizeof(c));
 
-printf("Size of long long int is : %lu byte(s)", (unsigned long)sizeof(e));
+printf("Size of long long int is : %lu byte(s)\n", (unsigned long)sizeof(e));
 return (0);
 
 }
