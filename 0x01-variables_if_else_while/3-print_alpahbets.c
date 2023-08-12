50c9289ec47f5f617b022e85591ccc75f70beeae
adams miracle <adamsmiracle0@gmail.com>
2023-08-12 17:34:58 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alpahbets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+  * main - Entry point
+  * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char c;
+	char b;
+
+	c = 'a';
+	b = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (b <= 'Z')
+	{
+		putchar(b)
+			b++
+	}
+	putchar('\n');
+	return (0);
+
+}
