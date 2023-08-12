a725b2f1f20bd9f20b3d48b31a0ec68643ccdec3
adams miracle <adamsmiracle0@gmail.com>
2023-08-12 00:51:12 +0000

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  *main - Prints something to the standard error
+  *Return: 0 (success)
+  */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (0);
+}
