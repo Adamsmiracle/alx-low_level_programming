c2b75a5770fd7477245ca24669ea80da7de7aa83
adams miracle <adamsmiracle0@gmail.com>
2023-08-19 01:31:34 +0000

    c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * main - test function for positive or negative
+  * Return: 0 (Success)
+  */
+
+int main(void)
+{
+	int i;
+
+	 i = 0;
+	if (c == 'q' || c == 'e')
+	 positive_or_negative(i);
+
+	return (0);
+}
