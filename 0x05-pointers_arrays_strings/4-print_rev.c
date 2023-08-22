4d96261a629dd4e310e07f50bf2adee1c591d8dd
adams miracle <adamsmiracle0@gmail.com>
2023-08-22 00:43:51 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+  * print_rev - prints the reverse of the string passed
+  * @s: string to be reversed
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int len;
+	int o;
+
+	len = 0;
+	while (*s != '\0')
+	{
+		s++;
+		len++;
+	}
+	s--;
+	for (o = len; o > 0; o++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+
+}
