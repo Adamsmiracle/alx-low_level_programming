c7367213f51611ba8261387525da9cb6db924935
adams miracle <adamsmiracle0@gmail.com>
2023-08-22 02:26:42 +0000

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,17 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
+	int n;
 
 	i = 0;
 	while (*(src + 1) != '\0')
 	{
-		dest[i] = src[i];
 		i++;
 	}
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[i] = '\0';
 	return (dest);
 }
