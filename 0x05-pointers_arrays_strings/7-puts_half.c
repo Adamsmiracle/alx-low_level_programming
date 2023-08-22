c64633db5eee34273e5612feca332601d24220cd
adams miracle <adamsmiracle0@gmail.com>
2023-08-22 01:45:44 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,5 +7,26 @@
 
 void puts_half(char *str)
 {
+	int i, a, len;
+
+	len = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		len++
+	}
+
+	n = (len / 2);
+
+	if (len % 2 == 1)
+	{
+		n = ((longi + 1) / 2);
+	}
+
+	for (a = n; str[a] != 0; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
 
 }
