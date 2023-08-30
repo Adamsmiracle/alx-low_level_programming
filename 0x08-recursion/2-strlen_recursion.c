2edafd8f100e6faa5d6e5d3d9c2fe023a0a0d595
adams miracle <adamsmiracle0@gmail.com>
2023-08-30 17:30:30 +0000

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _strlen_recursion - prinst the len of  the string using recursion
+  * @s: input string
+  * Return: length of the string
+  */
+
+int _strlen_recursion(char *s)
+{
+	int len;
+
+	len = 0;
+	if (*s)
+	{
+		len++;
+		len += _strlen_recursion(s + 1)
+	}
+	return (len)
+}
