3572906f10fea4f6391a115a742ca1da72e5d611
adams miracle <adamsmiracle0@gmail.com>
2023-09-01 20:59:34 +0000

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - returns the first argument
+  * @argc: argument count
+  * @argv: argument vector
+  * Return: 0 (Success)
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+  * _putchar - prints the character to the standard output
+  * @c: input character
+  * Return: 0 (Success)
+  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
