891dbaa85c03cfe0224ca26d2b2fff751442a9c4
adams miracle <adamsmiracle0@gmail.com>
2023-09-04 23:30:21 +0000

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,6 +18,12 @@ char *str_concat(char *s1, char *s2)
 	while (s1[len1] != '\0')
 		len1++;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
