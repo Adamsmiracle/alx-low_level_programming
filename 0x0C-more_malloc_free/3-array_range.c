70f03191962fcd683a29149f34e6015f1b0bcd4c
adams miracle <adamsmiracle0@gmail.com>
2023-09-07 14:54:59 +0000

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+  * array_range - prints the integer array from the min to the max
+  * @min: minimum value;
+  * @max: maximum value;
+  * Return: pointer to the array
+  */
+int *array_range(int min, int max)
+{
+	int *p;
+	int i;
+	size_t size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	P = malloc(sizeof(int) * size);
+
+	if (!p)
+		return (NULL);
+
+	for (i = 0; min <= max; i++)
+	{
+		p[i] = min++;
+	}
+
+	return (p);
+}
