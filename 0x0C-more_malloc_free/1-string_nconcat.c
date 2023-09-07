3a7f1ba04699ce6a9047a010999c7956d4df2988
adams miracle <adamsmiracle0@gmail.com>
2023-09-07 14:13:31 +0000

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,15 +10,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *p;
-	unsigned int len1;
-	unsigned int len2;
-	unsigned int i;
-	unsigned int j;
-
-	i = 0;
-	len1 = 0;
-	len2 = 0;
-	j = 0;
+	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 
 	while (s1 && s1[len1])
 		len1++;
@@ -46,7 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		p[i++] = s2[j++];
 	}
 
-	while (n >= len2 && 1 < (len1 + n))
+	while (n >= len2 && 1 < (len1 + len2))
 		p[i++] = s2[j++];
 	p[i] = '\0';
 
