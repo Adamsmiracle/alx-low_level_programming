881660bb60aa2de0fe1f4048946620e78eb5f06e
adams miracle <adamsmiracle0@gmail.com>
2023-09-11 17:21:34 +0000

    task 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,8 +17,7 @@ void print_dog(struct dog *d)
 
 	if (d->owner == NULL)
 		d->owner = "(nil)";
-	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age,#!/bin/bash
-			echo "\"(Ôo)'"
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 		"
 
 }
