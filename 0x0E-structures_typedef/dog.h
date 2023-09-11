01fa8dc22c45df5f4924e6d7f78f4b10196e51ce
adams miracle <adamsmiracle0@gmail.com>
2023-09-11 15:47:22 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * dog - structure about the details of a dog
+  * @name: member
+  * @owner: member
+  * @age: member
+*/
+
+struct dog 
+{
+	char *name;
+	char *owner;
+	int age;
+};
