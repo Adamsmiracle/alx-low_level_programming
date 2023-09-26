caec8ecabeaa44f5a5c8ee470a9cab3ad93fd2c7
adams miracle <adamsmiracle0@gmail.com>
2023-09-26 11:45:56 +0000

    task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-	listint_t *current;
+	listint_t *current = NULL;
 	unsigned int i;
 
 	i = 0;
@@ -25,6 +25,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
+		if (!temp || !(temp->next))
+			return (-1);
 		temp = temp->next;
 		i++;
 	}
