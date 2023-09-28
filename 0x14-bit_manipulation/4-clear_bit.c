e619f5baabffa801611373deb8c98db49a9b19a8
adams miracle <adamsmiracle0@gmail.com>
2023-09-28 23:41:33 +0000

    task 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * clear_bit - set the bit to 0
+ * @n: the to set the bit in
+ * @index: the index of the bit
+ * Return: 1 success else -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = (1 << index) & 0;
+
+	return (1);
+}
