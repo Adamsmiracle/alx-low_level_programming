cd1ec474d70a805d08bb59279a2fcc4c159f774d
adams miracle <adamsmiracle0@gmail.com>
2023-11-10 14:22:20 +0000

    task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -3,23 +3,18 @@
  * get_dnodeint_at_index - gets  the node at a specific index
  * @head: head of the linked list
  * @index: index of the node
- * Return: the node 
+ * Return: the node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    dlistint_t *current = head;
+	dlistint_t *current = head;
+	unsigned int l = 0;
 
-    while (current != NULL && i < index) {
-        current = current->next;
-        i++;
-    }
+	while (current != NULL && i < index)
+	{
+		current = current->next;
+		i++;
+	}
 
-    // Check if the node at the given index exists
-    if (current == NULL) {
-        return NULL;
-    }
-
-    return current;
-
+	return (current);
 }
