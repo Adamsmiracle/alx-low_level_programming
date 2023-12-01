497d612fcc8a1b5111ace5a9b507aeb2ff77e074
adams miracle <adamsmiracle0@gmail.com>
2023-12-01 17:49:09 +0000

    task 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gets index for a key given
+ * @key: key 
+ * @size: size of the table
+ *
+ * Return: size of key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
