70b2da24a9f7a1b362612e6ff4128d23d348eda7
adams miracle <adamsmiracle0@gmail.com>
2023-12-02 08:54:09 +0000

    task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints all the elements of the hash table
+ * @ht: pointer to the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *bucket;
+	int not_final = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		bucket = ht->array[i];
+
+		while (bucket)
+		{
+			if (not_final)
+				printf(",");
+			printf("'%s': '%s'", bucket->key, bucket->value);
+			not_final = 1;
+			bucket = bucket->next;
+		}
+	}
+
+	printf("}");
+}
