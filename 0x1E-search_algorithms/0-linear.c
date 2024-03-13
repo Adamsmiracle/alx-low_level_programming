c9dc7bab36f11f9f98eaff58ee0896049d9d4318
adams miracle <adamsmiracle0@gmail.com>
2024-03-13 16:24:31 +0000

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include "search_algos.h"
 /**
  * linear_search - sequential search algorithm
  * @array: the data structure
@@ -11,17 +12,16 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		if (!array)
-			return (-1);
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+
 		if (array[i] == value)
 			return (i);
 
-		if (i == size)
-		{
-			return (-1);
-		}
 	}
 
 	return (-1);
