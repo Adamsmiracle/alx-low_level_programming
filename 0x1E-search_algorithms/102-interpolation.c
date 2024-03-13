dc744f58e69025c22940515ffbe8cd78ad1b27b9
adams miracle <adamsmiracle0@gmail.com>
2024-03-13 22:11:01 +0000

    interpolation

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -14,6 +14,8 @@ int interpolation_search(int *array, size_t size, int value)
 
 	if (array == NULL || size == 0)
 		return (-1);
+	low = 0;
+	high = size - 1;
 
 	while (array[high] != array[low] &&
 			(value >= array[low]) && (value <= array[high]))
